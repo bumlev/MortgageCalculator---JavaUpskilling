8f165715ac03270fde4332472e8b5b1979f09b25
bumlev <bumwelevy@yahoo.in>
2024-02-09 00:36:02 +0200

    get the monthly payment

--- src/Execution.java
+++ src/Execution.java
@@ -2,5 +2,35 @@ import java.util.Scanner;
 
 public class Execution {
 
+    public static void main(String[] args) {
 
+        MortgageCalculator mortgageCalculator = new MortgageCalculator();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome in Mortgage Calculator");
+        System.out.println("Please enter the loan amount in dollars: ");
+
+        double amountOfLoan = InputException.inputExceptionForDouble(scanner);
+        mortgageCalculator.setAmountOfLoan(amountOfLoan);
+
+        System.out.println("Please enter the annual interest rate as a percentage: ");
+        double annualInterestRate = InputException.inputExceptionForDouble(scanner);
+        double monthlyInterestRate = annualInterestRate/12/100;
+        mortgageCalculator.setMonthlyInterestRate(monthlyInterestRate);
+
+
+        System.out.println("Please enter the loan duration in years: ");
+        int years = InputException.inputExceptionForInteger(scanner);
+        int numberOfPayments = years * 12;
+        mortgageCalculator.setTotalNumberOfPayments(numberOfPayments);
+
+        double monthlyPayment  = mortgageCalculator.getAmountOfLoan() * mortgageCalculator.getMonthlyInterestRate() *
+                Math.pow(1 + mortgageCalculator.getMonthlyInterestRate() , numberOfPayments) /
+                (Math.pow(1 + mortgageCalculator.getMonthlyInterestRate() ,
+                        mortgageCalculator.getTotalNumberOfPayments()) - 1);
+
+        System.out.println("The monthly payment is " + monthlyPayment);
+    }
 }
+
+
